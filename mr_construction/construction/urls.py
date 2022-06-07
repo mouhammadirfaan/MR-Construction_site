36cb9831fc89526eb78cc9184d9b5fe9586ba62a
MUHAMMAD IRFAN <mouhammadirfaan@gmail.com>
2022-06-07 21:25:31 +0500

    add

--- mr_construction/construction/urls.py
+++ mr_construction/construction/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-     path('', views.Index, name='index'),
-    # path('about', views.About, name='about'),
-    # path('prijects', views.Projdcts, name='projects'),
-    # path('contact', views.Contect, name='contect'), 
+    path('', views.Index, name='index'),
+    path('index/', views.Index, name='index'),
+    path('about/', views.About, name='about'),
+    path('prijects/', views.Projdcts, name='projects'),
+    path('contact/', views.Contect, name='contect'), 
 ]
+urlpatterns += staticfiles_urlpatterns()
