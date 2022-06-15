3cb300ec9b2cba9adcc4f4b72151720e71724ee9
MUHAMMAD IRFAN <mouhammadirfaan@gmail.com>
2022-06-15 22:52:24 +0500

    add changes

--- mr_construction/construction/views.py
+++ mr_construction/construction/views.py
@@ -17,17 +17,22 @@ def Projects(request):
     return render(request, 'construction/projects.html')
 
 def Contect(request):
-
     if request.method == "POST":
-        form = ContactForm(request.POST)
+        firtname = request.POST['first_name']
+        lastname = request.POST['last_name']
+        email = request.POST['email_address']
+        message = request.POST['text_area'] 
 
-        if form.is_valid():
-            firtname = form.cleaned_data['first_name']
-            lastname = form.cleaned_deta['last_name']
-            email = form.cleaned_deta['email_address']
-            message = form.cleaned_deta['text_area']
-    else:
+        form_variable = ContactForm(first_name=firtname, last_nam=lastname, email_address=email, text_area=message)
+    return render(request, 'construction/contact.html')
 
-        form = ContactForm()
-
-    return render(request, 'construction/contact.html', {"form":form})
+    # if request.method == "POST":
+    #     form = ContactForm(request.POST)
+    #     if form.is_valid():
+    #         firtname = form.cleaned_data['first_name']
+    #         lastname = form.cleaned_deta['last_name']
+    #         email = form.cleaned_deta['email_address']
+    #         message = form.cleaned_deta['text_area']
+    # else:
+    #     form = ContactForm()
+    # return render(request, 'construction/contact.html', {"form":form})
