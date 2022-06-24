a7a999466863f6081ef2de3ccd83836465bf040f
MUHAMMAD IRFAN <mouhammadirfaan@gmail.com>
2022-06-24 08:19:27 +0500

    add changes

--- mr_construction/construction/urls.py
+++ mr_construction/construction/urls.py
@@ -1,6 +1,6 @@
-from django.contrib import admin
+# from django.contrib import admin
 from django.urls import path
-from . import views
+from django.contrib.auth import views
 # from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
